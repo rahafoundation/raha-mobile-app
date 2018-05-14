1303da26291f8508ad43ac74da39b37ae721801d
Omar Diab <me@omardiab.com>
2018-05-13 21:05:05 -0700

    home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { Button } from "react-native-elements";
 
 import SearchBar from "../shared/SearchBar";
@@ -14,6 +14,7 @@ export default class Home extends React.Component<HomeProps> {
       <View style={styles.container}>
         <SearchBar />
         <Text>This is the home page.</Text>
+        <View style={styles.spacer} />
         <Button
           title="Go to LogIn"
           onPress={() => this.props.navigation.navigate("LogIn")}
@@ -25,9 +26,11 @@ export default class Home extends React.Component<HomeProps> {
 
 const styles = StyleSheet.create({
   container: {
+    minHeight: "100%",
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
+  },
+  spacer: {
+    flexGrow: 1
   }
 });
