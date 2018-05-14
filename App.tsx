123cd78096df41a56eaf074ec8ddf7b38231565e
Omar Diab <me@omardiab.com>
2018-05-13 18:10:34 -0700

    use react-navigator in app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,9 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { createStackNavigator } from "react-navigation";
+import Home from "./src/Home";
 
-export default class App extends React.Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.ts to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+export default createStackNavigator({
+  Home: {
+    screen: Home
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
