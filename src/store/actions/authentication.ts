a4e092c7be3a15f058a94fe45fb068ebca35b652
Omar Diab <me@omardiab.com>
2018-05-23 19:33:11 -0400

    make google sign in action

--- src/store/actions/authentication.ts
+++ src/store/actions/authentication.ts
@@ -1,12 +1,11 @@
 import { Action } from "redux";
 import firebase from "firebase";
 import { ActionCreator } from "redux";
-
-// TODO: this is native-specific, if we're splitting out the redux store,
-// figure out how to pull out platform-independent stuff like this.
-import { Google } from "expo";
+import { NavigationActions } from "react-navigation";
 
 import { AsyncActionCreator } from "./types";
+import { Google } from "expo";
+import { auth } from "../../firebaseInit";
 
 export const SET_FIREBASE_USER = "SET_FIREBASE_USER";
 
@@ -33,7 +32,16 @@ export const authSetFirebaseUser: AsyncActionCreator = (
  * mobile-specific login methods like this vs platform-independent ones.
  */
 export const googleLogIn: AsyncActionCreator = () => async dispatch => {
-  const googleData = await Google.logInAsync({});
+  const googleData = await Google.logInAsync({
+    iosClientId:
+      "1000788606610-poko6usql5gpdaq9bl0h3ljac8lnpprr.apps.googleusercontent.com",
+    iosStandaloneAppClientId:
+      "1000788606610-31qfhcdqisk618gndrhacfq1mvc5p7cc.apps.googleusercontent.com",
+    androidClientId:
+      "1000788606610-off9qgir4lgtgct5lrod948e4k28egfc.apps.googleusercontent.com",
+    androidStandaloneAppClientId:
+      "1000788606610-3si004egrf47c9em0hpn255v8kl8ebau.apps.googleusercontent.com"
+  });
   if (googleData.type === "cancel") {
     // TODO: dispatch something that says it was cancelled?
     return;
@@ -46,8 +54,8 @@ export const googleLogIn: AsyncActionCreator = () => async dispatch => {
     );
 
     // Login with the credential
-    const firebaseUser = await firebase.auth().signInWithCredential(credential);
-    dispatch(setFirebaseUser(firebaseUser));
+    const authData = await auth.signInAndRetrieveDataWithCredential(credential);
+    dispatch(setFirebaseUser(authData.user));
   } catch (error) {
     const { code, message } = error;
   }
