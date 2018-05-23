48734a9018d72dd846880dd7200f352c0ab69875
Omar Diab <me@omardiab.com>
2018-05-23 19:31:32 -0400

    make redux store with devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "remote-redux-devtools";
 
 import rootReducer from "./reducers";
 
 export default () => {
-  return createStore(rootReducer, applyMiddleware(thunk));
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 };
 
 export { AppState } from "./reducers";
