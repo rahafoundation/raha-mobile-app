e2f1cd305bbd298adc5a4dd096c43e83f45c3d0d
Omar Diab <me@omardiab.com>
2018-05-27 18:21:17 -0400

    port selector for getting amount of raha for member

---
+++ src/store/selectors/member.ts
@@ -0,0 +1,24 @@
+import Big from "big.js";
+
+import { RahaState } from "../";
+import { MemberId } from "../../identifiers";
+import { getMembersByUid } from "./members";
+
+const RAHA_UBI_WEEKLY_RATE = 10;
+const MILLISECONDS_PER_WEEK = 1000 * 60 * 60 * 24 * 7;
+
+export function getMemberMintableAmount(
+  state: RahaState,
+  memberId: MemberId
+): string | undefined {
+  const members = getMembersByUid(state, [memberId]);
+  const member = members && members.length > 0 ? members[0] : undefined;
+  if (member) {
+    return new Big(new Date().getTime() - member.lastMinted.getTime())
+      .div(MILLISECONDS_PER_WEEK)
+      .times(RAHA_UBI_WEEKLY_RATE)
+      .round(2, 0)
+      .toString();
+  }
+  return undefined;
+}
