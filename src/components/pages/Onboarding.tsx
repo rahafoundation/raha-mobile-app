36f963c37b5d22853ea95138ed74d049e39bb041
Tina Roh <tinaroh@gmail.com>
2018-05-29 16:16:20 -0700

    Add README to copy over config

--- src/components/pages/Onboarding.tsx
+++ src/components/pages/Onboarding.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import RoundedButton from "../shared/RoundedButton";
+import Swiper from "../shared/Swiper";
 
 type OnboardingProps = {
   navigation: any;
