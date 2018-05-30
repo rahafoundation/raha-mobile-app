57946bc4a15c4a09ecb6250479816ec3bafef59e
Tina Roh <tinaroh@gmail.com>
2018-05-30 16:04:21 -0700

    Add Expo Camera

--- src/components/pages/OnboardingCamera.tsx
+++ src/components/pages/OnboardingCamera.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { StyleSheet, View } from "react-native";
-import { RNCamera } from "react-native-camera";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { Camera, Permissions } from "expo";
 import { connect, MapDispatchToProps, MapStateToProps } from "react-redux";
 import {
   googleLogIn,
@@ -15,13 +15,67 @@ type StateProps = {};
 
 type OnboardingCameraProps = OwnProps & StateProps;
 
-const OnboardingCamera: React.StatelessComponent<
-  OnboardingCameraProps
-> = props => {
-  return <View style={styles.container} />;
-};
+class OnboardingCamera extends React.Component<OnboardingCameraProps> {
+  state = {
+    hasCameraPermission: null,
+    type: "front"
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === "granted" });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: "transparent",
+                flexDirection: "row"
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: "flex-end",
+                  alignItems: "center"
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === "back" ? "front" : "back"
+                  });
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+                >
+                  {" "}
+                  Flip{" "}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
 
 const styles = StyleSheet.create({
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
   container: {
     minHeight: "100%",
     flex: 1,
