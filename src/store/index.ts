971408a27c0b1fb99f0f52460f306814d2d72142
Omar Diab <me@omardiab.com>
2018-05-30 16:54:15 -0400

    use persistedReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,18 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "remote-redux-devtools";
+import { persistStore } from "redux-persist";
 
-import rootReducer, { RahaState } from "./reducers";
+import rootReducer, { RahaState } from "./persistedReducer";
 import { RahaAction } from "./actions";
 
 export default () => {
-  return createStore<RahaState, RahaAction, {}, {}>(
+  const store = createStore<RahaState, RahaAction, {}, {}>(
     rootReducer,
     composeWithDevTools(applyMiddleware(thunk))
   );
+  return persistStore(store);
 };
 
-export { RahaState } from "./reducers";
+export { RahaState } from "./persistedReducer";
 export { RahaAction } from "./actions";
