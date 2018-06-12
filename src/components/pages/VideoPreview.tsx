59b8e79e9e4329225a108d7e64a8c6418707f298
Tina Roh <tinaroh@gmail.com>
2018-06-12 15:27:29 -0700

    Add VideoPreview component

---
+++ src/components/pages/VideoPreview.tsx
@@ -0,0 +1,77 @@
+/**
+ * Renders the video camera preview screen after recording a video.
+ */
+
+import * as React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { Video } from "expo";
+import { RouteName } from "../../../App";
+
+type VideoPreviewProps = {
+  navigation: any;
+};
+
+export default class VideoPreview extends React.Component<VideoPreviewProps> {
+  render() {
+    const videoUri = this.props.navigation.getParam("videoUri", null);
+    if (videoUri) {
+      console.log(videoUri);
+      return (
+        <View style={styles.container}>
+          <Video
+            source={{
+              uri: videoUri
+            }}
+            rate={1.0}
+            volume={1.0}
+            isMuted={false}
+            usePoster={true}
+            resizeMode={Video.RESIZE_MODE_COVER}
+            shouldPlay
+            isLooping
+            // @ts-ignore
+            style={styles.video}
+          />
+          <Button
+            title="Upload Video"
+            onPress={() => {
+              this.uploadVideo();
+            }}
+          />
+          <Button
+            title="Retake"
+            onPress={() => {
+              this.props.navigation.navigate(RouteName.OnboardingCamera);
+            }}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text>Need a valid video. Please retry.</Text>
+          <Button
+            title="Retake"
+            onPress={() => {
+              this.props.navigation.navigate(RouteName.OnboardingCamera);
+            }}
+          />
+        </View>
+      );
+    }
+  }
+
+  uploadVideo = () => {
+    console.log("uploading video now");
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  video: {
+    width: "100%",
+    aspectRatio: 3 / 4
+  }
+});
