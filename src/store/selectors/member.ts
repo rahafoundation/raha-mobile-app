49d088aaa2f0d020e693edc8b771b6413f4827e2
Omar Diab <me@omardiab.com>
2018-06-12 20:53:25 -0400

    Uid -> Ids

--- src/store/selectors/member.ts
+++ src/store/selectors/member.ts
@@ -2,7 +2,7 @@ import Big from "big.js";
 
 import { RahaState } from "../";
 import { MemberId } from "../../identifiers";
-import { getMembersByUid } from "./members";
+import { getMembersByIds } from "./members";
 
 const RAHA_UBI_WEEKLY_RATE = 10;
 const MILLISECONDS_PER_WEEK = 1000 * 60 * 60 * 24 * 7;
@@ -11,7 +11,7 @@ export function getMemberMintableAmount(
   state: RahaState,
   memberId: MemberId
 ): string | undefined {
-  const members = getMembersByUid(state, [memberId]);
+  const members = getMembersByIds(state, [memberId]);
   const member = members && members.length > 0 ? members[0] : undefined;
   if (member) {
     return new Big(new Date().getTime() - member.lastMinted.getTime())
