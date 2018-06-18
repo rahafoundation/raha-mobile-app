e2adfc5d0d344107f3a430bca8e31608ebfbbc8a
Omar Diab <me@omardiab.com>
2018-06-17 17:56:16 -0700

    extraneous scrollview import

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-elements";
 import { connect, MapStateToProps } from "react-redux";
 
