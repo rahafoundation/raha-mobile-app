f772d5ccd441e5f5cb5b1ad5db89bf8f7c5596a7
Mark Ulrich <markulrich@users.noreply.github.com>
2018-06-20 16:13:15 -0700

    Add SafeAreaView

---
+++ src/shared/SafeAreaView.tsx
@@ -0,0 +1,29 @@
+/**
+ * The default react native SafeAreaView only works for iOS.
+ * This component works for iOS and Android to prevent View from overlapping
+ * with notifications bar or sensor area.
+ * Support for passing through props is limited and will probably have
+ * different effects on iOS/Android.
+ */
+import * as React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView as SafeAreaViewIosOnly,
+  ViewProps,
+  Platform,
+  StatusBar
+} from "react-native";
+
+export const SafeAreaView: React.StatelessComponent<ViewProps> = props => {
+  if (Platform.OS === "ios") {
+    return <SafeAreaViewIosOnly {...props} />;
+  }
+  const { children, ...remaining } = props;
+  return (
+    <View {...remaining}>
+      <View style={{ height: StatusBar.currentHeight }} />
+      <View {...props} />
+    </View>
+  );
+};
