5215c38395d436de3754fae0f5d12497c558d351
Mark Ulrich <markulrich@users.noreply.github.com>
2018-06-21 11:31:57 -0700

    Stateless Home.tsx

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,32 +5,20 @@ import { MemberSearchBar } from "../shared/MemberSearchBar";
 import { ActivityFeed } from "../shared/ActivityFeed";
 import { OperationType } from "../../store/reducers/operations";
 
-type Props = {
-  navigation: any;
+export const Home: React.StatelessComponent = () => {
+  return (
+    <View>
+      <Text>Give Raha to:</Text>
+      <MemberSearchBar
+        // Make onPress go to the item instead of dismissing keyboard
+        keyboardShouldPersistTaps="always"
+        onMemberSelected={member => {
+          console.log(member.username + " clicked");
+        }}
+      />
+      <ActivityFeed
+        filter={operation => operation.op_code !== OperationType.MINT}
+      />
+    </View>
+  );
 };
-
-class HomeView extends React.Component<Props> {
-  // componentDidMount() {
-  //   this.props
-  // }
-
-  render() {
-    return (
-      <View>
-        <Text>Give Raha to:</Text>
-        <MemberSearchBar
-          // Make onPress go to the item instead of dismissing keyboard
-          keyboardShouldPersistTaps="always"
-          onMemberSelected={member => {
-            console.log(member.username + " clicked");
-          }}
-        />
-        <ActivityFeed
-          filter={operation => operation.op_code !== OperationType.MINT}
-        />
-      </View>
-    );
-  }
-}
-
-export const Home = HomeView;
