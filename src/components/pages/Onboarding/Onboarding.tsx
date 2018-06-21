df50ee6cb0c208ee40a25c49977478453e39ef56
Tina Roh <tinaroh@gmail.com>
2018-06-20 17:44:12 -0700

    Connect the rest of onboarding with error messages

--- src/components/pages/Onboarding/Onboarding.tsx
+++ src/components/pages/Onboarding/Onboarding.tsx
@@ -94,11 +94,55 @@ export class OnboardingView extends React.Component<
         );
       }
       case OnboardingStep.CAMERA: {
+        // Shouldn't happen, but if any required field is cleared or the onboarding
+        // flow is screwed up, redirect to the correct step.
+        const verifiedFullName = this.state.verifiedName;
+        if (!verifiedFullName) {
+          this.setState({
+            errorMessage: "Need a verified name.",
+            step: OnboardingStep.VERIFY_NAME
+          });
+          break;
+        }
+        const invitingMemberName = this.state.invitingMember
+          ? this.state.invitingMember.fullName
+          : undefined;
+        if (!invitingMemberName) {
+          this.setState({
+            errorMessage: "Need to choose an inviter.",
+            step: OnboardingStep.CHOOSE_INVITER
+          });
+          break;
+        }
+
         return (
-          <ChooseInviter
-            onInviterSelected={member => {
+          <OnboardingCamera
+            inviterFullName={invitingMemberName}
+            verifiedFullName={verifiedFullName}
+            onVideoRecorded={videoUri => {
               this.setState({
-                invitingMember: member,
+                videoUri: videoUri,
+                step: OnboardingStep.VIDEO_PREVIEW
+              });
+            }}
+          />
+        );
+      }
+      case OnboardingStep.VIDEO_PREVIEW: {
+        const videoUri = this.state.videoUri;
+        if (!videoUri) {
+          this.setState({
+            errorMessage: "Invalid video. Please try again.",
+            step: OnboardingStep.CAMERA
+          });
+          break;
+        }
+        return (
+          <VideoPreview
+            videoUri={videoUri}
+            retakeVideo={(errorMessage?: string) => {
+              this.setState({
+                errorMessage: errorMessage,
                 step: OnboardingStep.CAMERA
               });
             }}
@@ -106,12 +150,17 @@ export class OnboardingView extends React.Component<
         );
       }
       default:
-        return undefined;
     }
+    return undefined;
   }
 
   render() {
-    return <View style={styles.container}>{this.renderOnboardingStep()}</View>;
+    return (
+      <View style={styles.container}>
+        {this.state.errorMessage && <Text>{this.state.errorMessage}</Text>}
+        {this.renderOnboardingStep()}
+      </View>
+    );
   }
 }
 
