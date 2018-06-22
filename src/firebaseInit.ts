f8ddc01ab6ddb1f437bbbf1f3d8d31c829339bc5
Omar Diab <me@omardiab.com>
2018-06-22 03:27:43 -0700

    set up new firebase

--- src/firebaseInit.ts
+++ src/firebaseInit.ts
@@ -1,6 +1,13 @@
-import * as firebase from "firebase";
+import firebase from "react-native-firebase";
 
 import { config } from "./data/config";
+import { Platform } from "react-native";
 
-export const app = firebase.initializeApp(config.firebase);
+if (Platform.OS !== "android" && Platform.OS !== "ios") {
+  throw new Error("Only supports android and ios");
+}
+
+export const app = firebase.initializeApp(config.firebase[Platform.OS], "raha");
+// export const app = firebase;
 export const auth = app.auth();
+export const storage = app.storage();
