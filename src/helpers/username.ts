0db8545b7be8719ae6bf9411a7c1b472e79c99b1
Tina Roh <tinaroh@gmail.com>
2018-06-21 23:19:28 -0700

    Add username helper. Removed normalizing accents

---
+++ src/helpers/username.ts
@@ -0,0 +1,23 @@
+const NUMBER_SUFFIX_LENGTH = 4;
+
+const getNumberSuffix = (len: number, seedFn: () => number) => {
+  const exclusiveMax = Math.pow(10, len);
+  return (Math.floor(seedFn() * exclusiveMax) + exclusiveMax)
+    .toString()
+    .substring(1);
+};
+
+// See https://stackoverflow.com/questions/4328500/how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex/
+const removePunctuation = (str: string) => {
+  return str.replace(/[,\/#!$%\^&\*;:{}=\_`~()]/g, "");
+};
+
+// TODO move server side
+export const getUsername = (displayName: string, seedFn?: () => number) => {
+  const userSlug = removePunctuation(displayName)
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, ".");
+  const userPin = getNumberSuffix(NUMBER_SUFFIX_LENGTH, seedFn || Math.random);
+  return userSlug + "." + userPin;
+};
