f7caaffcc2a1f0c6156de9ae212735320d743795
Omar Diab <me@omardiab.com>
2018-06-22 03:27:57 -0700

    add new firebase data to config

--- src/data/prod.config.ts
+++ src/data/prod.config.ts
@@ -2,12 +2,30 @@ export const config = {
   apiBase: "https://raha-5395e.appspot.com/api/",
   publicVideoBucket: "raha-video",
   firebase: {
-    apiKey: "AIzaSyBXuACowZcLcr1wlhM53LHtibFwa59EmAY",
-    authDomain: "app.raha.io",
-    databaseURL: "https://raha-5395e.firebaseio.com",
-    projectId: "raha-5395e",
-    storageBucket: "raha-5395e.appspot.com",
-    messagingSenderId: "677137485282"
+    ios: {
+      apiKey: "AIzaSyCMp31nMIR5KMJegtRIVT3y7aX71sadysY",
+      authDomain: "app.raha.io",
+      clientId:
+        "677137485282-hei8t5jm6c67kpqk4objefcrlk4bhuqd.apps.googleusercontent.com",
+      databaseURL: "https://raha-5395e.firebaseio.com",
+      projectId: "raha-5395e",
+      storageBucket: "raha-5395e.appspot.com",
+      messagingSenderId: "677137485282",
+      appId: "1:677137485282:ios:4023b851b124d320",
+      persistence: true
+    },
+    android: {
+      clientId:
+        "677137485282-o8enpde66k4rdppkmemh9k7l8gu71sbi.apps.googleusercontent.com",
+      appId: "1:677137485282:android:4023b851b124d320",
+      authDomain: "app.raha.io",
+      apiKey: "AIzaSyBvR-LBe_hWy-HzyE1X4OkbEyALOr7COzo",
+      databaseURL: "https://raha-5395e.firebaseio.com",
+      storageBucket: "raha-5395e.appspot.com",
+      messagingSenderId: "677137485282",
+      projectId: "raha-5395e",
+      persistence: true
+    }
   },
   facebook: {
     appId: "239560396587704"
