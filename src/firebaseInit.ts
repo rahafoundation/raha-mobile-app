27470630e820e0ff28d982cf67717f1e84bcef96
Mark Ulrich <markulrich@users.noreply.github.com>
2018-07-24 00:34:05 -0700

    Simply use firebase.app() for android

--- src/firebaseInit.ts
+++ src/firebaseInit.ts
@@ -1,4 +1,4 @@
-import RNFirebase from "react-native-firebase";
+import firebase from "react-native-firebase";
 
 import { config } from "./data/config";
 import { Platform } from "react-native";
@@ -7,9 +7,17 @@ if (Platform.OS !== "android" && Platform.OS !== "ios") {
   throw new Error("Only supports android and ios");
 }
 
-export const app = RNFirebase.initializeApp(
-  config.firebase[Platform.OS],
-  "raha"
-);
+function getApp() {
+  if (Platform.OS !== "android") {
+    return firebase.app();
+  } else {  // TODO check if above also works for ios
+    return firebase.initializeApp(
+      config.firebase[Platform.OS],
+      "raha"
+    );
+  }
+}
+
+export const app = getApp();
 export const auth = app.auth();
 export const storage = app.storage();
