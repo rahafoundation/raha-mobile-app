141315617a65da0fdcb0820a8f45ac019b0df341
Mark Ulrich <markulrich@users.noreply.github.com>
2018-08-04 12:07:08 -0700

    Remove TODO

--- src/components/pages/PendingInvites.tsx
+++ src/components/pages/PendingInvites.tsx
@@ -3,35 +3,32 @@ import { Map } from "immutable";
 import { connect, MapStateToProps } from "react-redux";
 
 import { MemberId } from "@raha/api-shared/dist/models/identifiers";
-import { ActivityFeed, isUnconfirmedRequestInvite } from "../shared/ActivityFeed";
+import {
+  ActivityFeed,
+  isUnconfirmedRequestInvite
+} from "../shared/ActivityFeed";
 import { Container } from "../shared/elements";
-import { RahaState } from '../../store';
-import { Member } from '../../store/reducers/members';
+import { RahaState } from "../../store";
+import { Member } from "../../store/reducers/members";
 
 type StateProps = {
-  membersById: Map<MemberId, Member>
-}
+  membersById: Map<MemberId, Member>;
+};
 
 const PendingInvitesView: React.StatelessComponent<StateProps> = props => {
   return (
     <Container>
       <ActivityFeed
-        filter={operation => isUnconfirmedRequestInvite(props.membersById, operation)}
+        filter={operation =>
+          isUnconfirmedRequestInvite(props.membersById, operation)
+        }
       />
     </Container>
   );
 };
 
-const mapStateToProps: MapStateToProps<
-  StateProps,
-  {},
-  RahaState
-> = state => {
+const mapStateToProps: MapStateToProps<StateProps, {}, RahaState> = state => {
   return { membersById: state.members.byMemberId };
 };
 
-// TODO show a notification with all Unconfirmed request invite relevant to your account
-// TODO show this Unconfirmed page buried somewhere (Account?) where people can view (and perhaps flag) all unconfirmed REQUEST_INVITE if they wish
-export const PendingInvites = connect(
-  mapStateToProps
-)(PendingInvitesView);
+export const PendingInvites = connect(mapStateToProps)(PendingInvitesView);
