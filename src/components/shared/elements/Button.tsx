9c09db7e7c489fe6645a8a67d3ddbc286bc4d3d3
Omar Diab <me@omardiab.com>
2018-08-18 16:49:19 +0900

    stop using react-native-elements button, restyle

--- src/components/shared/elements/Button.tsx
+++ src/components/shared/elements/Button.tsx
@@ -1,32 +1,84 @@
 import * as React from "react";
-import { TouchableOpacity, StyleSheet } from "react-native";
-import { Button as NativeButton, ButtonProps } from "react-native-elements";
+import {
+  StyleSheet,
+  ViewStyle,
+  TouchableOpacity,
+  StyleProp,
+  TextStyle,
+  Text
+} from "react-native";
 import { fonts } from "../../../helpers/fonts";
 import { colors } from "../../../helpers/colors";
 
+// TODO: add size, color options
+interface ButtonProps {
+  title: string;
+  onPress: () => void;
+  style?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<TextStyle>;
+  disabledStyle?: StyleProp<ViewStyle>;
+  disabledTextStyle?: StyleProp<TextStyle>;
+  disabled?: boolean;
+}
+
 export const Button: React.StatelessComponent<ButtonProps> = props => {
+  const {
+    title,
+    onPress,
+    style,
+    textStyle,
+    disabled,
+    disabledTextStyle,
+    disabledStyle
+  } = props;
+
+  const disabledStyles = [
+    styles.disabled,
+    ...(disabledStyle ? [disabledStyle] : [])
+  ];
+  const disabledTextStyles = [
+    styles.disabledText,
+    ...(disabledTextStyle ? [disabledTextStyle] : [])
+  ];
+
   return (
-    <TouchableOpacity {...props} style={[props.style]}>
-      <NativeButton
-        textStyle={fonts.Lato.Bold}
-        {...props}
-        buttonStyle={styles.button}
-        disabledStyle={styles.disabledColor}
-      />
+    <TouchableOpacity
+      style={[styles.button, style, ...(disabled ? disabledStyles : [])]}
+      onPress={onPress}
+      disabled={disabled}
+    >
+      <Text
+        style={[
+          styles.text,
+          textStyle,
+          ...(disabled ? disabledTextStyles : [])
+        ]}
+      >
+        {title.toUpperCase()}
+      </Text>
     </TouchableOpacity>
   );
 };
 
+const buttonStyle: ViewStyle = {
+  backgroundColor: colors.button,
+  borderRadius: 2,
+  paddingVertical: 10,
+  paddingHorizontal: 20
+};
+
+const disabledStyle: ViewStyle = {
+  backgroundColor: colors.disabledButton
+};
+
+const textStyle: TextStyle = {
+  ...fonts.Lato.Bold
+};
+const disabledTextStyle: TextStyle = {};
+
 const styles = StyleSheet.create({
-  buttonContainer: {
-    backgroundColor: colors.button,
-    borderRadius: 3
-  },
-  disabledColor: {
-    backgroundColor: colors.disabledButton
-  },
-  button: {
-    backgroundColor: colors.button,
-    borderRadius: 3
-  }
+  text: textStyle,
+  button: buttonStyle,
+  disabled: disabledStyle,
+  disabledText: disabledTextStyle
 });
