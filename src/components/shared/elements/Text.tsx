67cfd766d561c05d0ace6ee190eb4b15d2910715
Omar Diab <me@omardiab.com>
2018-08-18 22:14:59 +0900

    make text component support compound content, including currencies

--- src/components/shared/elements/Text.tsx
+++ src/components/shared/elements/Text.tsx
@@ -7,14 +7,90 @@ import {
   Text as NativeText,
   TextProps,
   TextStyle,
-  StyleSheet
+  StyleSheet,
+  StyleProp
 } from "react-native";
 
 import { fonts, fontSizes } from "../../../helpers/fonts";
 import { colors } from "../../../helpers/colors";
+import { Currency, CurrencyValue } from "../Currency";
 
-export const Text: React.StatelessComponent<TextProps> = props => {
-  return <NativeText {...props} style={[styles.text, props.style]} />;
+export type CompoundContent = (string | CurrencyValue)[];
+
+function renderCompoundContent(
+  compoundContent: {
+    textTransform?: (text: string) => string;
+    content: CompoundContent;
+  },
+  textStyle: StyleProp<TextStyle>
+) {
+  const { content, textTransform } = compoundContent;
+  const transformed = textTransform
+    ? compoundContent.content.map(
+        c => (typeof c === "string" ? textTransform(c) : c)
+      )
+    : content;
+  return (
+    transformed
+      // Render each piece of the description
+      .map((piece, idx) => {
+        const key = `content-${idx}`;
+        if (typeof piece === "string") {
+          return (
+            <NativeText style={textStyle} key={key}>
+              {piece}
+            </NativeText>
+          );
+        }
+        if ("currencyType" in piece) {
+          return <Currency style={textStyle} key={key} currencyValue={piece} />;
+        }
+        console.error(
+          "Unexpected value in compoundContent:",
+          piece,
+          ". Omitting."
+        );
+        return undefined;
+      })
+      // remove undefined pieces (i.e. errors)
+      .filter(x => !!x)
+      // Put spaces between each component
+      .reduce(
+        (memo, nextComponent, idx) => [
+          ...memo,
+          <NativeText style={textStyle} key={`spacing-${idx}`}>
+            {" "}
+          </NativeText>,
+          nextComponent
+        ],
+        [] as React.ReactNode[]
+      )
+  );
+}
+
+/**
+ * General purpose text component for the app. Includes a special
+ * compoundContent key that lets you seamlessly render currency values alongside
+ * other text.
+ */
+export const Text: React.StatelessComponent<
+  TextProps & {
+    compoundContent?: {
+      textTransform?: (text: string) => string;
+      content: CompoundContent;
+    };
+  }
+> = props => {
+  const { children, compoundContent, ...rest } = props;
+  const textStyle: StyleProp<TextStyle> = [styles.text, props.style];
+  const renderedChildren = compoundContent
+    ? renderCompoundContent(compoundContent, textStyle)
+    : children;
+  return (
+    <NativeText {...rest} style={textStyle}>
+      {renderedChildren}
+    </NativeText>
+  );
 };
 
 const textStyle: TextStyle = {
