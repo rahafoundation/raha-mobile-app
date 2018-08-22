df6bcd5a33eaa94c5c554f53a81c50b73c7f3080
Omar Diab <me@omardiab.com>
2018-08-22 19:47:47 +0900

    fix behavior of safe area view for ios, remove unnecessary padding for android

--- src/components/shared/SafeAreaView.tsx
+++ src/components/shared/SafeAreaView.tsx
@@ -1,27 +1,29 @@
 /**
- * The default react native SafeAreaView only works for iOS.
- * This component works for iOS and Android to prevent View from overlapping
- * with notifications bar or sensor area.
- * Support for passing through props is limited and will probably have
- * different effects on iOS/Android.
+ * When used at the top level, produces a view that doesn't overlap with iOS
+ * and Android status and navigation bars.
  */
 import * as React from "react";
-import {
-  View,
-  SafeAreaView as SafeAreaViewIosOnly,
-  ViewProps,
-  Platform,
-  StatusBar
-} from "react-native";
+import { View, ViewProps, Platform } from "react-native";
 
-export const SafeAreaView: React.StatelessComponent<ViewProps> = props => {
+import { SafeAreaView as SafeAreaViewIosOnly } from "react-navigation";
+
+export const SafeAreaView: React.StatelessComponent<{}> = ({ children }) => {
+  // iOS
   if (Platform.OS === "ios") {
-    return <SafeAreaViewIosOnly {...props} />;
+    return (
+      <SafeAreaViewIosOnly
+        forceInset={{
+          top: "always",
+          left: "always",
+          bottom: "always",
+          right: "always"
+        }}
+      >
+        {children}
+      </SafeAreaViewIosOnly>
+    );
   }
-  const { children, ...remaining } = props;
-  return (
-    <View style={{ paddingTop: 100, paddingBottom: 100 }} {...remaining}>
-      <View {...props} />
-    </View>
-  );
+
+  // Android
+  return <View>{children}</View>;
 };
