856178d8d03449e839eed46ed7f4ba4f7aecbcb2
Tina Roh <tinaroh@gmail.com>
2018-09-06 15:32:26 +0900

    Add processDeeplink as util function

---
+++ src/components/shared/Deeplinking.tsx
@@ -0,0 +1,23 @@
+import { NavigationActions } from "react-navigation";
+import url from "url";
+import { DEEPLINK_ROUTES } from "./Navigation";
+
+export function processDeeplink(link: string, navigation: any) {
+  const deeplinkUrl = url.parse(link, true, true);
+  if (!deeplinkUrl.pathname) {
+    return;
+  }
+  const pathname = deeplinkUrl.pathname.replace(
+    "/",
+    ""
+  ) as keyof typeof DEEPLINK_ROUTES;
+  debugger;
+  navigation.navigate(
+    "App",
+    {},
+    NavigationActions.navigate({
+      routeName: DEEPLINK_ROUTES[pathname],
+      params: deeplinkUrl.query
+    })
+  );
+}
