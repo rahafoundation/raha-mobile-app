cd979290b06ac8082693ebe9f2af286ae72a59c8
Tina Roh <tinaroh@users.noreply.github.com>
2018-09-09 18:32:41 +0900

    Allow user to enter an invite token manually. (#286)
    * Trim whitespace on email
    
    * Add InputInviteToken
    
    * Revert testing
    
    * Don't auto-advance to Camera if required fields are missing
    
    * Add a comment
    
    * CR

---
+++ src/components/pages/Onboarding/InputInviteToken.tsx
@@ -0,0 +1,111 @@
+import * as React from "react";
+import { View } from "react-native";
+
+import {
+  Button,
+  Text,
+  TextInput,
+  IndependentPageContainer
+} from "../../shared/elements";
+import { styles } from "./styles";
+import { connect, MapStateToProps } from "react-redux";
+import { RahaState } from "../../../store";
+import { InvitationsState } from "../../../store/reducers/invitations";
+import { styles as sharedStyles } from "./styles";
+import { colors } from "react-native-elements";
+
+/**
+ * Page that requests an invite token
+ */
+
+type OwnProps = {
+  onInputInviteToken: (inviteToken: string) => any;
+  onBack: () => any;
+};
+
+type ReduxStateProps = {
+  doesInviteTokenExist: (t: string) => boolean;
+};
+
+type InputInviteTokenProps = OwnProps & ReduxStateProps;
+
+type InputInviteTokenState = {
+  inviteToken?: string;
+  errorMessage?: string;
+};
+
+const mapStateToProps: MapStateToProps<
+  ReduxStateProps,
+  OwnProps,
+  RahaState
+> = state => {
+  return {
+    doesInviteTokenExist: (token: string) =>
+      state.invitations.byInviteToken.has(token)
+  };
+};
+
+class InputInviteTokenView extends React.Component<
+  InputInviteTokenProps,
+  InputInviteTokenState
+> {
+  state: InputInviteTokenState = {};
+
+  validateInviteToken() {
+    const token = this.state.inviteToken;
+    if (!token) {
+      return;
+    }
+
+    if (!this.props.doesInviteTokenExist(token)) {
+      this.setState({
+        errorMessage: "This code is invalid or has expired."
+      });
+      return;
+    }
+
+    this.props.onInputInviteToken(token);
+  }
+
+  render() {
+    return (
+      <IndependentPageContainer containerStyle={styles.cardPageContainer}>
+        <View style={styles.page}>
+          <Text style={styles.back} onPress={this.props.onBack}>
+            Back
+          </Text>
+          <View style={styles.body}>
+            <View style={styles.card}>
+              <Text style={{ fontSize: 18 }}>
+                Please enter your invite token:
+              </Text>
+              <TextInput
+                placeholder={"abcxyz123"}
+                onChangeText={text =>
+                  this.setState({
+                    inviteToken: text ? text.trim() : undefined,
+                    errorMessage: undefined
+                  })
+                }
+                value={this.state.inviteToken}
+              />
+              {this.state.errorMessage && (
+                <Text style={[{ color: colors.error }, sharedStyles.paragraph]}>
+                  {this.state.errorMessage}
+                </Text>
+              )}
+              <Button
+                title={`Confirm`}
+                disabled={this.state.inviteToken === undefined}
+                onPress={() => {
+                  this.validateInviteToken();
+                }}
+              />
+            </View>
+          </View>
+        </View>
+      </IndependentPageContainer>
+    );
+  }
+}
+export const InputInviteToken = connect(mapStateToProps)(InputInviteTokenView);
--- src/components/pages/Onboarding/OnboardingCamera.tsx
+++ src/components/pages/Onboarding/OnboardingCamera.tsx
@@ -1,17 +1,18 @@
 /**
- * Renders the onboarding camera preview screen which allows a user to record their
+ * Renders the onboarding camera preview component which allows a user to record their
  * identity verification video.
  */
 
 import * as React from "react";
 import { StyleSheet, View, TextStyle } from "react-native";
 import { Camera } from "../../shared/Camera";
-import { Text, IndependentPageContainer } from "../../shared/elements";
+import { Text } from "../../shared/elements";
 import { fonts } from "../../../helpers/fonts";
 import { styles as sharedStyles } from "./styles";
 
 type OwnProps = {
   verifiedFullName: string;
+  inviterFullName?: string;
   onVideoRecorded: (videoUri: string) => void;
 };
 type OnboardingCameraProps = OwnProps;
@@ -19,28 +20,29 @@ type OnboardingCameraProps = OwnProps;
 export class OnboardingCamera extends React.Component<OnboardingCameraProps> {
   render() {
     return (
-      <IndependentPageContainer>
-        <View style={sharedStyles.page}>
+      <React.Fragment>
+        <Text style={sharedStyles.paragraph}>
+          {this.props.inviterFullName
+            ? "Please record a video of yourself stating your identity to accept your invite from " +
+              this.props.inviterFullName
+            : "Please record a video of yourself stating your identity."}
+        </Text>
+        <Camera
+          onVideoRecorded={uri => {
+            this.props.onVideoRecorded(uri);
+          }}
+        />
+        <View>
+          <Text style={sharedStyles.paragraph}>Example of what to say:</Text>
           <Text style={sharedStyles.paragraph}>
-            Please record a public video of yourself stating your identity.
+            "My name is{" "}
+            <Text style={[sharedStyles.paragraphText, styles.name]}>
+              {this.props.verifiedFullName}
+            </Text>{" "}
+            and I'm joining Raha because I believe every life has value."
           </Text>
-          <Camera
-            onVideoRecorded={uri => {
-              this.props.onVideoRecorded(uri);
-            }}
-          />
-          <View>
-            <Text style={sharedStyles.paragraph}>Example of what to say:</Text>
-            <Text style={sharedStyles.paragraph}>
-              "My name is{" "}
-              <Text style={[sharedStyles.paragraphText, styles.name]}>
-                {this.props.verifiedFullName}
-              </Text>{" "}
-              and I'm joining Raha because I believe every life has value."
-            </Text>
-          </View>
         </View>
-      </IndependentPageContainer>
+      </React.Fragment>
     );
   }
 }
