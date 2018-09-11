1cbba8fde61541fad2033d85b1b1290496a0a3b0
Omar Diab <me@omardiab.com>
2018-09-11 12:34:40 +0900

    Add custom keyboardawarescrollview with default props

---
+++ src/components/shared/elements/KeyboardAwareScrollView.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import {
+  KeyboardAwareScrollView as KeyboardAwareScrollViewLibrary,
+  KeyboardAwareScrollViewProps
+} from "react-native-keyboard-aware-scroll-view";
+import { Platform } from "react-native";
+
+/**
+ * KeyboardAwareScrollView that passes through all provided properties like the
+ * original component, but also sets sensible defaults for our project.
+ *
+ * On Android, behavior is different than iOS regarding how the container
+ * scales; Hence, there are platform differences in the props provided by
+ * default.
+ */
+export const KeyboardAwareScrollView: React.StatelessComponent<
+  KeyboardAwareScrollViewProps
+> = props => {
+  const platformProps = Platform.OS === "android" ? { flexGrow: 1 } : {};
+  return (
+    <KeyboardAwareScrollViewLibrary
+      enableAutomaticScroll
+      bounces={false}
+      enableOnAndroid
+      {...platformProps}
+      {...props}
+    />
+  );
+};
