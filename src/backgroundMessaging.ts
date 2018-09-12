e000298e5ca790896a8e020bdde57b7ad20d3a15
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-09-13 00:46:18 +0900

    Remove currently unused background messaging callbacks.

--- src/backgroundMessaging.ts
+++
@@ -1,8 +0,0 @@
-import firebase from "react-native-firebase";
-import { RemoteMessage } from "react-native-firebase/messaging";
-
-// Must return within 60 seconds.
-// See: https://rnfirebase.io/docs/v4.3.x/messaging/receiving-messages#1)-Add-Service-to-Android-Manifest
-export default async (message: RemoteMessage) => {
-  return Promise.resolve();
-};
