847b9505f79ee8a59f6530e940a72f0fd894f415
Tina Roh <tinaroh@gmail.com>
2018-09-13 23:46:49 +0900

    Auto login notifies user

--- src/components/AuthManager.tsx
+++ src/components/AuthManager.tsx
@@ -1,21 +1,32 @@
 import * as React from "react";
-import { connect } from "react-redux";
+import { connect, MapStateToProps } from "react-redux";
 
 import { auth } from "../firebaseInit";
 import { logInAction, signedOutAction } from "../store/actions/authentication";
 import { RNFirebase } from "react-native-firebase";
 import { Dispatch } from "redux";
+import DropdownAlert from "react-native-dropdownalert";
+import { RahaState } from "../store";
 
+type ReduxStateProps = {
+  wasAutoLoggedIn?: boolean;
+};
 type OwnProps = { children: React.ReactNode };
 type DispatchProps = {
   logIn: () => any;
   signOut: () => any;
 };
-type Props = OwnProps & DispatchProps;
+type AuthManagerProps = OwnProps & DispatchProps & ReduxStateProps;
 
-class AuthManagerComponent extends React.Component<Props> {
+/**
+ * Component that manages login state when the user is logged in automatically
+ * by Firebase and notifies the user that happened.
+ */
+class AuthManagerComponent extends React.Component<AuthManagerProps> {
   private unsubscribe?: () => void;
-  public constructor(props: Props) {
+  private dropdown: any;
+
+  public constructor(props: AuthManagerProps) {
     super(props);
     this.unsubscribe = undefined;
   }
@@ -34,8 +45,23 @@ class AuthManagerComponent extends React.Component<Props> {
     this.unsubscribe && this.unsubscribe();
   }
 
+  public componentDidUpdate() {
+    if (this.props.wasAutoLoggedIn) {
+      this.dropdown.alertWithType(
+        "success",
+        "Auto Logged In",
+        "Google Play Services logged you in automatically."
+      );
+    }
+  }
+
   public render() {
-    return this.props.children;
+    return (
+      <React.Fragment>
+        {this.props.children}
+        <DropdownAlert ref={(ref: any) => (this.dropdown = ref)} />
+      </React.Fragment>
+    );
   }
 }
 
@@ -46,7 +72,17 @@ function mapDispatchToProps(dispatch: Dispatch, ownProps: OwnProps) {
   };
 }
 
+const mapStateToProps: MapStateToProps<
+  ReduxStateProps,
+  OwnProps,
+  RahaState
+> = state => {
+  return {
+    wasAutoLoggedIn: state.authentication.wasAutoLoggedIn
+  };
+};
+
 export const AuthManager = connect(
-  undefined,
+  mapStateToProps,
   mapDispatchToProps
 )(AuthManagerComponent);
--- src/store/actions/authentication.ts
+++ src/store/actions/authentication.ts
@@ -1,4 +1,4 @@
-import { RNFirebase as FirebaseTypes } from "react-native-firebase";
+import firebase, { RNFirebase as FirebaseTypes } from "react-native-firebase";
 import { ActionCreator } from "redux";
 
 import { validateMobileNumber as callValidateMobileNumber } from "@raha/api/dist/me/validateMobileNumber";
@@ -19,6 +19,8 @@ export const enum PhoneLogInActionType {
 }
 export const enum FirebaseAuthActionType {
   LOG_IN = "AUTHENTICATION.LOG_IN",
+  // Action used by only on Android when Firebase logs in automatically.
+  AUTO_LOG_IN = "AUTHENTICATION.AUTO_LOG_IN",
   SIGN_OUT = "AUTHENTICATION.SIGN_OUT",
   SIGNED_OUT = "AUTHENTICATION.SIGNED_OUT"
 }
@@ -28,6 +30,10 @@ export type AuthenticationActionType =
 
 export interface LogInAction {
   type: FirebaseAuthActionType.LOG_IN;
+}
+
+export interface AutoLogInAction {
+  type: FirebaseAuthActionType.AUTO_LOG_IN;
 }
 
 export interface SignOutAction {
@@ -54,12 +60,19 @@ export type PhoneLogInAction =
 
 export type AuthenticationAction =
   | LogInAction
+  | AutoLogInAction
   | SignOutAction
   | SignedOutAction
   | PhoneLogInAction;
 
 export const logInAction: ActionCreator<LogInAction> = (): LogInAction => ({
   type: FirebaseAuthActionType.LOG_IN
+});
+
+export const autoLogInAction: ActionCreator<
+  AutoLogInAction
+> = (): AutoLogInAction => ({
+  type: FirebaseAuthActionType.AUTO_LOG_IN
 });
 
 const signOutAction: ActionCreator<SignOutAction> = (): SignOutAction => ({
@@ -74,24 +87,62 @@ export const cancelPhoneLogIn: ActionCreator<PhoneLogInAction> = () => ({
   type: PhoneLogInActionType.PHONE_LOGIN_CANCELED
 });
 
-let confirmResult: FirebaseTypes.ConfirmationResult;
+const _logInWithCredential = async (
+  verificationId: string,
+  verificationCode: string
+) => {
+  const credential = firebase.auth.PhoneAuthProvider.credential(
+    verificationId,
+    verificationCode
+  );
+  await firebase.auth().signInAndRetrieveDataWithCredential(credential);
+};
+
+let verificationId: string;
 export const initiatePhoneLogIn: AsyncActionCreator = (
   phoneNumber: string
 ) => async dispatch => {
+  dispatch({
+    type: PhoneLogInActionType.PHONE_LOGIN_SENDING_PHONE_NUMBER
+  });
+
+  await callValidateMobileNumber(config.apiBase, phoneNumber);
+
+  // Handle phone state events manually so that we can know when the user is
+  // logged in automatically.
   try {
-    dispatch({
-      type: PhoneLogInActionType.PHONE_LOGIN_SENDING_PHONE_NUMBER
-    });
-
-    await callValidateMobileNumber(config.apiBase, phoneNumber);
-
-    confirmResult = await auth.signInWithPhoneNumber(phoneNumber);
-    dispatch({
-      type: PhoneLogInActionType.PHONE_LOGIN_WAITING_FOR_CONFIRMATION_INPUT
-    });
+    await auth
+      .verifyPhoneNumber(phoneNumber)
+      .on("state_changed", async snapshot => {
+        switch (snapshot.state) {
+          case firebase.auth.PhoneAuthState.CODE_SENT:
+            // Save verification ID to log in after code input.
+            verificationId = snapshot.verificationId;
+            dispatch({
+              type:
+                PhoneLogInActionType.PHONE_LOGIN_WAITING_FOR_CONFIRMATION_INPUT
+            });
+            break;
+          case firebase.auth.PhoneAuthState.ERROR:
+            if (snapshot.error) {
+              throw snapshot.error;
+            }
+            break;
+          case firebase.auth.PhoneAuthState.AUTO_VERIFIED:
+            // This only happens on Android phones. Log in automatically.
+            if (snapshot.code) {
+              verificationId = snapshot.verificationId;
+              await _logInWithCredential(verificationId, snapshot.code);
+              dispatch({
+                type: FirebaseAuthActionType.AUTO_LOG_IN
+              });
+            }
+            break;
+        }
+      });
   } catch (err) {
-    // TODO: this is probably not what we want to display
     let errorMessage = err.message;
+    // TODO: this is probably not what we want to display
     // get API returned error message if parseable
     // TODO: generate messages based on the error code
     if (err instanceof ApiCallFailedError) {
@@ -108,7 +159,6 @@ export const initiatePhoneLogIn: AsyncActionCreator = (
       errorMessage
     });
     console.error("Initiating phone log in failed", errorMessage);
-    return;
   }
 };
 
@@ -119,7 +169,9 @@ export const confirmPhoneLogIn: AsyncActionCreator = (
     dispatch({
       type: PhoneLogInActionType.PHONE_LOGIN_SENDING_CONFIRMATION
     });
-    await confirmResult.confirm(confirmationCode);
+    await _logInWithCredential(verificationId, confirmationCode);
+    dispatch({ type: FirebaseAuthActionType.LOG_IN });
+    // await confirmResult.confirm(confirmationCode);
     // no need to dispatch success since firebase auth is listening for changes,
     // it will get triggered automatically
   } catch (err) {
