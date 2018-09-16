bba8df004a1339d8ab343bcac0720cff3200ee3b
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-09-16 15:00:46 +0900

    Add notification listener for iOS.

--- ios/Raha/AppDelegate.m
+++ ios/Raha/AppDelegate.m
@@ -34,7 +34,29 @@
   // Initialize Firebase
   [FIRApp configure];
   [RNFirebaseNotifications configure];
-
+  
+  // Notification listener registration as specified at https://firebase.google.com/docs/cloud-messaging/ios/client.
+  if ([UNUserNotificationCenter class] != nil) {
+    // iOS 10 or later
+    // For iOS 10 display notification (sent via APNS)
+    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+    UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
+    UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+    [[UNUserNotificationCenter currentNotificationCenter]
+     requestAuthorizationWithOptions:authOptions
+     completionHandler:^(BOOL granted, NSError * _Nullable error) {
+       // ...
+     }];
+  } else {
+    // iOS 10 notifications aren't available; fall back to iOS 8-9 notifications.
+    UIUserNotificationType allNotificationTypes =
+    (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+    UIUserNotificationSettings *settings =
+    [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+    [application registerUserNotificationSettings:settings];
+  }
+  
+  [application registerForRemoteNotifications];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Raha"
