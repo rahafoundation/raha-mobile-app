5d65c6b2f86d8f3bf95a091d6a24707c9b698bba
Omar Diab <me@omardiab.com>
2018-10-03 19:57:05 +0900

    mock api in test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -3,6 +3,15 @@ import { App } from "./App";
 
 import * as renderer from "react-test-renderer";
 
+jest.mock("@raha/api/dist/callApi", () => ({
+  callApi: (...args: any[]) => {
+    console.info("Called API endpoint with args:", args);
+    return Promise.resolve({
+      body: []
+    });
+  }
+}));
+
 it("renders without crashing", () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
