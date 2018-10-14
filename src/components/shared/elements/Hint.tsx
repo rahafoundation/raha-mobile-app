f9ae14533f7be9f07f5ca3eb7e69c70446043423
Tina Roh <tinaroh@gmail.com>
2018-10-14 17:20:38 +0900

    Add Hint component

---
+++ src/components/shared/elements/Hint.tsx
@@ -0,0 +1,88 @@
+import * as React from "react";
+import Modal from "react-native-modal";
+import {
+  TouchableOpacity,
+  View,
+  ScrollView,
+  StyleSheet,
+  TextStyle
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { Text } from "./Text";
+import { colors } from "../../../helpers/colors";
+import { fonts } from "../../../helpers/fonts";
+import { Button } from "./Button";
+
+export enum IconType {
+  // Default; "i" icon when we're providing the user with detailed/additional information
+  INFO = "info-circle",
+
+  // "?" icon when it directly clarifies a user question
+  QUESTION = "question-circle"
+}
+
+type HintProps = {
+  type?: IconType;
+  text: string;
+};
+
+type HintState = {
+  isModalVisible: boolean;
+};
+
+export class Hint extends React.Component<HintProps, HintState> {
+  constructor(props: HintProps) {
+    super(props);
+    this.state = { isModalVisible: false };
+  }
+
+  _toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Modal
+          isVisible={this.state.isModalVisible}
+          onBackdropPress={this._toggleModal}
+          onBackButtonPress={this._toggleModal}
+        >
+          <View style={styles.modalContent}>
+            <ScrollView>
+              <Text style={{ margin: 8 }}>{this.props.text}</Text>
+              <Button title="Close" onPress={this._toggleModal} />
+            </ScrollView>
+          </View>
+        </Modal>
+        <TouchableOpacity style={styles.hintIcon} onPress={this._toggleModal}>
+          <Icon
+            name={this.props.type ? this.props.type : IconType.INFO}
+            size={20}
+            color={colors.modalIcon}
+          />
+        </TouchableOpacity>
+      </React.Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  hintIcon: {
+    marginRight: 8
+  },
+  modalContent: {
+    padding: 22,
+    backgroundColor: colors.modalBackground,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4
+  },
+  modalCloseButton: {
+    marginTop: 8
+  },
+  text: {
+    flexGrow: 1,
+    ...fonts.Lato.Normal
+  } as TextStyle
+});
