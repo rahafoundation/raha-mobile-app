23e17712347657ff36a7b88c7013db349088f6a9
Tina Roh <tinaroh@gmail.com>
2018-10-17 17:21:33 +0900

    Ignore dependency react-native-animatable in Jest

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // entirely using jest.mock() in a setup file, you need to tell jest to ignore
   // them, since native code won't be present when running jest in the terminal.
   transformIgnorePatterns: [
-    "node_modules/(?!(react-native|react-navigation-tabs|react-native-vector-icons|react-native-paper|react-navigation-tabs|react-native-elements|react-native-video-processing|react-native-video|react-navigation|react-native-country-picker-modal|react-native-safe-area-view|react-native-device-info|react-native-dropdownalert|react-native-camera|react-native-code-push|react-native-branch|react-native-keyboard-aware-scroll-view|react-native-iphone-x-helper|react-native-svg|react-native-app-settings|react-native-modal)/)"
+    "node_modules/(?!(react-native|react-navigation-tabs|react-native-vector-icons|react-native-paper|react-navigation-tabs|react-native-elements|react-native-video-processing|react-native-video|react-navigation|react-native-country-picker-modal|react-native-safe-area-view|react-native-device-info|react-native-dropdownalert|react-native-camera|react-native-code-push|react-native-branch|react-native-keyboard-aware-scroll-view|react-native-iphone-x-helper|react-native-svg|react-native-app-settings|react-native-modal|react-native-animatable)/)"
   ],
   setupFiles: [
     "<rootDir>/scripts/jest/setup",
