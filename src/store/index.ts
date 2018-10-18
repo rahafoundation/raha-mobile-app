9e4aed20e29d7cad6db3364b54e18b15c0e09aed
Mark Ulrich <6357579+markulrich@users.noreply.github.com>
2018-10-18 00:20:58 -0700

    Log auth actions and group active operation creation (#421)

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,6 +8,15 @@ import { rootReducer, RahaState } from "./persistedReducer";
 import { RahaAction } from "./actions";
 import { analytics } from "../firebaseInit";
 import { ApiCallsActionType } from "./actions/apiCalls";
+import {
+  FirebaseAuthActionType,
+  PhoneLogInActionType
+} from "./actions/authentication";
+import { ApiEndpointName } from "@raha/api-shared/dist/routes/ApiEndpoint";
+
+// Analytics event names
+const ACTIVE_OPERATION_CREATOR = "ACTIVE_OPERATION_CREATOR";
+export const VIEW_WEB_LINK = "VIEW_WEB_LINK";
 
 const API_CALL_LOGGED_PROPS = ImmutableSet(["endpoint"]);
 
@@ -17,14 +26,40 @@ const ACTION_TYPES_TO_LOGGED_PROPS = ImmutableMap([
   [ApiCallsActionType.FAILURE, API_CALL_LOGGED_PROPS]
 ]) as ImmutableMap<string, ImmutableSet<string>>;
 
+const ACTION_TYPES_TO_LOG = ImmutableSet([
+  ...Object.values(PhoneLogInActionType),
+  ...Object.values(FirebaseAuthActionType)
+]);
+
+const ACTIVATE_OPERATIONS = ImmutableSet([
+  ApiEndpointName.SEND_INVITE,
+  ApiEndpointName.GIVE,
+  ApiEndpointName.MINT,
+  ApiEndpointName.VERIFY_MEMBER,
+  ApiEndpointName.TRUST_MEMBER
+]);
+
+function isActivateOpCreator(action: RahaAction) {
+  return (
+    action.type === ApiCallsActionType.SUCCESS &&
+    ACTIVATE_OPERATIONS.has(action.endpoint)
+  );
+}
+
 const logger: Middleware = store => next => action => {
   let logged_props = ACTION_TYPES_TO_LOGGED_PROPS.get(action.type);
-  if (!!logged_props) {
-    const to_log = Object.keys(action)
-      .filter(key => logged_props && logged_props.has(key))
-      .reduce((obj, key) => ({ ...obj, [key]: action[key] }), {});
-    const event = action.type.replace(".", "_");
-    analytics.logEvent(event, to_log);
+  const event = action.type && action.type.replace(".", "_");
+  if (logged_props) {
+    const params = logged_props.reduce(
+      (obj, key) => ({ ...obj, [key]: action[key] }),
+      {}
+    );
+    analytics.logEvent(event, params);
+  } else if (ACTION_TYPES_TO_LOG.has(action.type)) {
+    analytics.logEvent(event);
+  }
+  if (isActivateOpCreator(action)) {
+    analytics.logEvent(ACTIVE_OPERATION_CREATOR);
   }
   return next(action);
 };
