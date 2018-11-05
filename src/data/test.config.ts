d80a33538a0503860fa639ee536186626863ec13
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-11-05 20:44:24 +0900

    Add current version info to config files.

--- src/data/test.config.ts
+++ src/data/test.config.ts
@@ -2,6 +2,11 @@ import { Config } from "./prod.config";
 
 export const config: Config = {
   apiBase: "https://raha-test.appspot.com/api/",
+  versionInfo: {
+    appVersion: "0.1.3",
+    iosCodepushVersion: 8,
+    androidCodepushVersion: 12
+  },
   publicVideoBucket: "raha-video-test",
   firebase: {
     ios: {
