f6d37a3f5bd8eacf904b991cd5f46c1b69ce8493
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-11-05 16:26:12 +0900

    Simplify input invite token text.

--- src/components/pages/Onboarding/InputInviteToken.tsx
+++ src/components/pages/Onboarding/InputInviteToken.tsx
@@ -13,10 +13,14 @@ import { RahaState } from "../../../store";
 import { styles as sharedStyles } from "./styles";
 import { colors } from "react-native-elements";
 
+enum InviteCodeStep {
+  ASK_IF_INVITED,
+  ENTER_INVITE_CODE
+}
+
 /**
  * Page that requests an invite token
  */
-
 type OwnProps = {
   onInputInviteToken: (inviteToken: string) => any;
   onContinueWithoutToken: () => any;
@@ -30,6 +34,7 @@ type ReduxStateProps = {
 type InputInviteTokenProps = OwnProps & ReduxStateProps;
 
 type InputInviteTokenState = {
+  step: InviteCodeStep;
   inviteToken?: string;
   errorMessage?: string;
 };
@@ -49,7 +54,7 @@ class InputInviteTokenView extends React.Component<
   InputInviteTokenProps,
   InputInviteTokenState
 > {
-  state: InputInviteTokenState = {};
+  state: InputInviteTokenState = { step: InviteCodeStep.ASK_IF_INVITED };
 
   validateInviteToken() {
     const token = this.state.inviteToken;
@@ -67,6 +72,75 @@ class InputInviteTokenView extends React.Component<
     this.props.onInputInviteToken(token);
   }
 
+  _renderStep() {
+    switch (this.state.step) {
+      case InviteCodeStep.ASK_IF_INVITED: {
+        return (
+          <React.Fragment>
+            <Text style={{ fontSize: 18 }}>
+              Were you invited to Raha by an existing member?
+            </Text>
+            <Button
+              title="Yes"
+              style={styles.button}
+              onPress={() => {
+                this.setState({ step: InviteCodeStep.ENTER_INVITE_CODE });
+              }}
+            />
+            <Button
+              title="Continue without invite"
+              style={styles.button}
+              onPress={this.props.onContinueWithoutToken}
+            />
+          </React.Fragment>
+        );
+      }
+      case InviteCodeStep.ENTER_INVITE_CODE: {
+        return (
+          <React.Fragment>
+            <Text style={{ fontSize: 18 }}>
+              Please enter the invite token you received in your invite email:
+            </Text>
+            <TextInput
+              autoFocus={true}
+              placeholder={"abcxyz123"}
+              onChangeText={text =>
+                this.setState({
+                  inviteToken: text ? text.trim() : undefined,
+                  errorMessage: undefined
+                })
+              }
+              value={this.state.inviteToken}
+              style={styles.input}
+            />
+            {this.state.errorMessage && (
+              <Text style={[{ color: colors.error }, sharedStyles.paragraph]}>
+                {this.state.errorMessage}
+              </Text>
+            )}
+            <Button
+              title="Confirm invite token"
+              disabled={this.state.inviteToken === undefined}
+              style={styles.button}
+              onPress={() => {
+                this.validateInviteToken();
+              }}
+            />
+            <Button
+              title="Continue without invite token"
+              disabled={this.state.inviteToken !== undefined}
+              style={styles.button}
+              onPress={this.props.onContinueWithoutToken}
+            />
+          </React.Fragment>
+        );
+      }
+      default: {
+        throw new Error(`Unknown invite code step: ${this.state.step}.`);
+      }
+    }
+  }
+
   render() {
     return (
       <IndependentPageContainer containerStyle={styles.cardPageContainer}>
@@ -75,43 +149,7 @@ class InputInviteTokenView extends React.Component<
             Back
           </Text>
           <View style={styles.body}>
-            <View style={styles.card}>
-              <Text style={{ fontSize: 18 }}>
-                If you were invited by an existing Raha member, please enter the
-                invite token you received in your invite email:
-              </Text>
-              <TextInput
-                autoFocus={true}
-                placeholder={"abcxyz123"}
-                onChangeText={text =>
-                  this.setState({
-                    inviteToken: text ? text.trim() : undefined,
-                    errorMessage: undefined
-                  })
-                }
-                value={this.state.inviteToken}
-                style={styles.input}
-              />
-              {this.state.errorMessage && (
-                <Text style={[{ color: colors.error }, sharedStyles.paragraph]}>
-                  {this.state.errorMessage}
-                </Text>
-              )}
-              <Button
-                title="Confirm invite token"
-                disabled={this.state.inviteToken === undefined}
-                style={styles.button}
-                onPress={() => {
-                  this.validateInviteToken();
-                }}
-              />
-              <Button
-                title="Continue without invite token"
-                disabled={this.state.inviteToken !== undefined}
-                style={styles.button}
-                onPress={this.props.onContinueWithoutToken}
-              />
-            </View>
+            <View style={styles.card}>{this._renderStep()}</View>
           </View>
         </View>
       </IndependentPageContainer>
