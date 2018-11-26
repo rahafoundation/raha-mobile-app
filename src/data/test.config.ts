92eda66f3141d25ef6d0f7642cc9fbc6afeca339
Tina Roh <tinaroh@gmail.com>
2018-11-26 07:27:39 -0800

    v0.2.1

--- src/data/test.config.ts
+++ src/data/test.config.ts
@@ -3,7 +3,7 @@ import { Config } from "./prod.config";
 export const config: Config = {
   apiBase: "https://raha-test.appspot.com/api/",
   versionInfo: {
-    appVersion: "0.2.0",
+    appVersion: "0.2.1",
     iosCodepushVersion: 10,
     androidCodepushVersion: 14
   },
